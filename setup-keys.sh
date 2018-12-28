49b6857b52b9029a698dc499f52f5c2b784ad107
Phill <Phill>
2018-12-28 22:41:43 +0000

    Add further steps in bash script to create everythng required for a self signed certificate

--- setup-keys.sh
+++ setup-keys.sh
@@ -2,8 +2,12 @@
 set -e;
 
 KEYS_DIR="target/keys/remote-server"
-PRIVATE_KEY="$KEYS_DIR/private-key.pem"
-PUBLIC_KEY="$KEYS_DIR/public-key.pem"
+PRIVATE_KEY="$KEYS_DIR/1-private-key.pem"
+CERTIFICATE_SIGNING_REQUEST="$KEYS_DIR/2-certificate-signing-request.csr"
+X509_CERT="$KEYS_DIR/3-x509-cert.pem"
+PRIVATE_KEY_AND_X509_CERT="$KEYS_DIR/4-private-key-and-x509-cert.pem"
+PKCS_KEY_STORE="$KEYS_DIR/5-keystore.pkcs12"
+#PUBLIC_KEY="$KEYS_DIR/public-key.pem"
 
 #Function that executes a command and prints it first - Thanks to Soth: https://stackoverflow.com/users/182619/soth, https://stackoverflow.com/questions/2853803/how-to-echo-shell-commands-as-they-are-executed
 executeAndPrint() { echo "\$ $@" ; "$@" ; }
@@ -11,8 +15,23 @@ executeAndPrint() { echo "\$ $@" ; "$@" ; }
 echo "Initialise directory for keys (if required)"
 mkdir -p ${KEYS_DIR}
 
-#Thanks to this page for commands: https://en.wikibooks.org/wiki/Cryptography/Generate_a_keypair_using_OpenSSL
 
-executeAndPrint openssl genpkey -pass pass:abcdefg -algorithm RSA -out ${PRIVATE_KEY} -pkeyopt rsa_keygen_bits:2048
+#Thanks to this page for commands: https://blog.knoldus.com/create-a-self-signed-ssl-certificate-using-openssl/
+#https://www.openssl.org/docs/man1.0.2/apps/openssl.html
 
-executeAndPrint openssl rsa -pubout -in ${PRIVATE_KEY} -out ${PUBLIC_KEY}
+echo "1. Generate a 2048 bit(recommended) RSA private key"
+executeAndPrint openssl genpkey -algorithm RSA -out ${PRIVATE_KEY} -pkeyopt rsa_keygen_bits:2048
+
+echo "2. Generate a CSR(Certificate Signing Request).  The CA will use the .csr file and issue the certificate, but in our case we will use this .csr file to create a self-signed certificate"
+executeAndPrint openssl req -config certificate-info.oid -new -key ${PRIVATE_KEY} -out ${CERTIFICATE_SIGNING_REQUEST}
+
+echo "3. Create the self-signed x509 certificate suitable for use on the web server"
+executeAndPrint openssl x509 -req -days 365 -in ${CERTIFICATE_SIGNING_REQUEST} -signkey ${PRIVATE_KEY} -out ${X509_CERT}
+
+echo "4. Create a new file with the private key and certificate"
+
+cat ${PRIVATE_KEY} > ${PRIVATE_KEY_AND_X509_CERT}
+cat ${X509_CERT} >> ${PRIVATE_KEY_AND_X509_CERT}
+
+echo "5. Create pkc12 file"
+executeAndPrint openssl pkcs12 -password pass:abcdefg -export -in ${PRIVATE_KEY_AND_X509_CERT} -out ${PKCS_KEY_STORE}
