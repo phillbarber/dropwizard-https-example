eead9abb371860178f54ad5ee0561a57aeb2cbc0
Phill <Phill>
2019-01-06 14:31:39 +0000

    Make acceptance test only start wiremock

--- src/test/java/com/github/phillbarber/HelloWorldAcceptanceTest.java
+++ src/test/java/com/github/phillbarber/HelloWorldAcceptanceTest.java
@@ -1,12 +1,12 @@
 package com.github.phillbarber;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.ClassRule;
+
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.ws.rs.core.Response;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
@@ -16,31 +16,25 @@ import static org.hamcrest.Matchers.is;
 
 public class HelloWorldAcceptanceTest {
 
-    @ClassRule
-    public final static DropwizardAppRule<AppConfig> appRule = new DropwizardAppRule<>(App.class);
+    public static final WireMockConfiguration OPTIONS = options()
+            .httpsPort(8000)
+            .keystorePath("/home/pergola/dev-workspace/dropwizard-https-example/target/keys/remote-server/5-keystore.pkcs12")
+            .keystoreType("PKCS12")
+            .keystorePassword("abcdefg");
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(
-            options()
-                    .port(8888)
-                    .httpsPort(8443)
-                    .keystorePath("/home/pergola/dev-workspace/dropwizard-https-example/target/keys/remote-server/5-keystore.pkcs12")
-                    .keystoreType("PKCS12")
-                    .keystorePassword("abcdefg")
+    public WireMockRule wireMockRule = new WireMockRule(OPTIONS);
 
-    );
-
+    public static void main(String[] args) {
+        new WireMockServer(OPTIONS).start();
+    }
 
     @Test
     public void hello() {
 
         wireMockRule.stubFor(get("/remote-service/message").willReturn(aResponse().withBody("Hello World!").withStatus(200)));
 
-        Response response = appRule.client().target("http://localhost:8080/message").request().get();
 
-        Object entity = response.readEntity(String.class);
-
-        assertThat(entity, is("Hello World!"));
 
     }
 }
