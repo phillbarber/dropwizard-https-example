069d050cdfaac1a1f03937a5807e26c22f080e92
Phill <Phill>
2019-01-06 14:39:40 +0000

    More accurate description of x509 cert

--- setup-keys.sh
+++ setup-keys.sh
@@ -4,7 +4,7 @@ set -e;
 KEYS_DIR="target/keys/remote-server"
 PRIVATE_KEY="$KEYS_DIR/1-private-key.pem"
 CERTIFICATE_SIGNING_REQUEST="$KEYS_DIR/2-certificate-signing-request.csr"
-X509_CERT="$KEYS_DIR/3-x509-cert.pem"
+PUBLIC_X509_CERT="$KEYS_DIR/3-x509-cert.pem"
 PRIVATE_KEY_AND_X509_CERT="$KEYS_DIR/4-private-key-and-x509-cert.pem"
 PKCS_KEY_STORE="$KEYS_DIR/5-keystore.pkcs12"
 #PUBLIC_KEY="$KEYS_DIR/public-key.pem"
@@ -25,13 +25,13 @@ executeAndPrint openssl genpkey -algorithm RSA -out ${PRIVATE_KEY} -pkeyopt rsa_
 echo "2. Generate a CSR(Certificate Signing Request).  The CA will use the .csr file and issue the certificate, but in our case we will use this .csr file to create a self-signed certificate"
 executeAndPrint openssl req -config certificate-info.oid -new -key ${PRIVATE_KEY} -out ${CERTIFICATE_SIGNING_REQUEST}
 
-echo "3. Create the self-signed x509 certificate suitable for use on the web server"
-executeAndPrint openssl x509 -req -days 365 -in ${CERTIFICATE_SIGNING_REQUEST} -signkey ${PRIVATE_KEY} -out ${X509_CERT}
+echo "3. Create the self-signed x509 public certificate suitable for use on the web server and (potentially) clients using the server"
+executeAndPrint openssl x509 -req -days 365 -in ${CERTIFICATE_SIGNING_REQUEST} -signkey ${PRIVATE_KEY} -out ${PUBLIC_X509_CERT}
 
 echo "4. Create a new file with the private key and certificate"
 
 cat ${PRIVATE_KEY} > ${PRIVATE_KEY_AND_X509_CERT}
-cat ${X509_CERT} >> ${PRIVATE_KEY_AND_X509_CERT}
+cat ${PUBLIC_X509_CERT} >> ${PRIVATE_KEY_AND_X509_CERT}
 
 echo "5. Create pkc12 file"
 executeAndPrint openssl pkcs12 -password pass:abcdefg -export -in ${PRIVATE_KEY_AND_X509_CERT} -out ${PKCS_KEY_STORE}
