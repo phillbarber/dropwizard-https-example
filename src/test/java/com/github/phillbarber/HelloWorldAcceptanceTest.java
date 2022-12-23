64dea13b40f642a01485883bf1a066354d926239
Phill Barber <phillbarber@github.com>
2022-12-23 08:46:44 +0000

    Upgrade to junit 5 and remove wiremock - acceptance test still failing

--- src/test/java/com/github/phillbarber/HelloWorldAcceptanceTest.java
+++ src/test/java/com/github/phillbarber/HelloWorldAcceptanceTest.java
@@ -1,84 +1,35 @@
 package com.github.phillbarber;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.common.JettySettings;
-import com.github.tomakehurst.wiremock.core.Options;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.http.AdminRequestHandler;
-import com.github.tomakehurst.wiremock.http.HttpServer;
-import com.github.tomakehurst.wiremock.http.HttpServerFactory;
-import com.github.tomakehurst.wiremock.http.StubRequestHandler;
-import com.github.tomakehurst.wiremock.jetty9.JettyHttpServer;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-
 import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import wiremock.org.eclipse.jetty.io.NetworkTrafficListener;
-import wiremock.org.eclipse.jetty.server.ConnectionFactory;
-import wiremock.org.eclipse.jetty.server.ServerConnector;
-import wiremock.org.eclipse.jetty.server.SslConnectionFactory;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.*;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+;
 
+@ExtendWith(DropwizardExtensionsSupport.class)
 public class HelloWorldAcceptanceTest {
 
-    public static final WireMockConfiguration OPTIONS = options()
-            .httpsPort(8000)
-
-            .keystorePath("/home/pergola/dev-workspace/dropwizard-https-example/target/keys/server/5-keystore-privatekey-and-publiccert.pkcs12")
-            .keystoreType("PKCS12")
-            .keystorePassword("abcdefg")
-
-            .trustStorePath("/home/pergola/dev-workspace/dropwizard-https-example/target/keys/client/6-keystore-publiccert-only.pkcs12")
-            .trustStoreType("PKCS12")
-            .trustStorePassword("abcdefg")
-            .needClientAuth(true)
-
-            .httpServerFactory(new Pkcs12FriendlyHttpsServerFactory());
-
-
-    @ClassRule
-    public static final DropwizardAppRule<AppConfig> RULE =
-            new DropwizardAppRule<AppConfig>(App.class, ResourceHelpers.resourceFilePath("config.yml"));
-
-    @ClassRule
-    public WireMockRule wireMockRule = new WireMockRule(OPTIONS);
-
+    private static DropwizardAppExtension<AppConfig> EXT = new DropwizardAppExtension<>(
+            App.class,
+            ResourceHelpers.resourceFilePath("config.yml")
+    );
 
     @Test
-    public void hello() {
+    void loginHandlerRedirectsAfterPost() {
+        Client client = EXT.client();
 
-        wireMockRule.stubFor(get("/remote-service/message").willReturn(aResponse().withBody("Hello World!").withStatus(200)));
+        Response response = client.target(
+                        String.format("https://localhost:%d/login", EXT.getLocalPort()))
+                .request()
+                .get();
 
-
-
+        assertNotNull(response);
     }
 
-    public static class Pkcs12FriendlyHttpsServerFactory implements HttpServerFactory {
-        @Override
-        public HttpServer buildHttpServer(Options options, AdminRequestHandler adminRequestHandler, StubRequestHandler stubRequestHandler) {
-            return new JettyHttpServer(
-                    options,
-                    adminRequestHandler,
-                    stubRequestHandler
-            ) {
-                @Override
-                protected ServerConnector createServerConnector(String bindAddress, JettySettings jettySettings, int port, NetworkTrafficListener listener, ConnectionFactory... connectionFactories) {
-                    if (port == options.httpsSettings().port()) {
-                        SslConnectionFactory sslConnectionFactory = (SslConnectionFactory) connectionFactories[0];
-                        sslConnectionFactory.getSslContextFactory().setKeyStorePassword(options.httpsSettings().keyStorePassword());
-                        connectionFactories = new ConnectionFactory[] {sslConnectionFactory, connectionFactories[1]};
-                    }
-                    return super.createServerConnector(bindAddress, jettySettings, port, listener, connectionFactories);
-                }
-            };
-
-        }
-    }
 }
